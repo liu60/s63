67920177bc83c5f201ca84b1897ec94267d94813
yl19881202 <1191437760@qq.com>
2017-12-28 14:47:18 +0800

    123123

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <headers></headers>
+    <router-view/>
+    <footers></footers>
+  </div>
+</template>
+
+<script>
+  import headers from './components/header';
+  import footers from './components/footer';
+export default {
+  name: 'app',
+  components:{
+    headers,footers
+  }
+}
+</script>
+
+<style>
+
+</style>
