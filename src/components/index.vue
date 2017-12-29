756b688d9cf2bf54eaa4d8dc426885fbd4ad25e1
黄正 <huangzheng@dafycredit.com>
2017-12-29 14:43:18 +0800

    轮播图

--- src/components/index.vue
+++ src/components/index.vue
@@ -1,17 +1,24 @@
 <template>
   <div>
-    <el-carouse :interval="5000" arrow="always">
+    <el-carousel :interval="5000" arrow="always">
       <el-carousel-item v-for="item in imgUrl" :key="item">
-        <img style="width: 100%;height: 500px" :src="'./../assets/images/index/'+item">
+        <img style="width: 100%;height: 500px" :src="item">
       </el-carousel-item>
-    </el-carouse>
+    </el-carousel>
   </div>
 </template>
 
 <script>
+  import banner1 from '../assets/images/index/banner1.jpg'
+  import banner2 from '../assets/images/index/banner2.jpg'
+  import banner3 from '../assets/images/index/banner3.jpg'
   export default {
-    name:'index',
-    imgUrl:['banner1.jpg','banner2.jpg','banner3.jpg']
+    data (){
+    	return {
+        name:'index',
+        imgUrl:[banner1,banner2,banner3]
+      }
+    }
   }
 </script>
 
