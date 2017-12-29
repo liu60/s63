88fcd2f73147e13a493ee4c9b60938cb6384bf20
yl19881202 <1191437760@qq.com>
2017-12-29 14:25:56 +0800

    element

--- src/components/index.vue
+++ src/components/index.vue
@@ -1,10 +1,17 @@
 <template>
-  <div></div>
+  <div>
+    <el-carouse :interval="5000" arrow="always">
+      <el-carousel-item v-for="item in imgUrl" :key="item">
+        <img style="width: 100%;height: 500px" :src="'./../assets/images/index/'+item">
+      </el-carousel-item>
+    </el-carouse>
+  </div>
 </template>
 
 <script>
   export default {
-    name:'index'
+    name:'index',
+    imgUrl:['banner1.jpg','banner2.jpg','banner3.jpg']
   }
 </script>
 
